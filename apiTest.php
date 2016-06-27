331be1b75aa9ea20b3d51b1c821c11a5672dd5b0
Kiren Padayachee <Kiren.Padayachee@multichoice.co.za>
2016-06-27 23:14:38 +0200

    Added table of API's to API Test page

--- apiTest.php
+++ apiTest.php
@@ -1,6 +1,11 @@
 <?php
 
+include("mysqldb/mySqlDbInit.php");
+include("mysqldb/mySqlDbCrudOperations.php");
+
 $html = file_get_contents("apiTestHtmlTemplate.html");
 echo $html;
+
+echo MySqlDbCrudOperations::getResultSetAsHtmlString(MySqlDbCrudOperations::getResultSetForSelectAllFromHttpPairs());
 
 ?>
