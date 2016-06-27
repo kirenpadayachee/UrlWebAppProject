1ec42122f3f7a79be350bca1dc84aeb972dc3130
Kiren Padayachee <Kiren.Padayachee@multichoice.co.za>
2016-06-27 23:58:30 +0200

    Got ApiTest to work. Api calls going through Javascript properly.

--- apiTest.php
+++ apiTest.php
@@ -3,9 +3,11 @@
 include("mysqldb/mySqlDbInit.php");
 include("mysqldb/mySqlDbCrudOperations.php");
 
+echo MySqlDbCrudOperations::getResultSetAsHtmlString(MySqlDbCrudOperations::getResultSetForSelectAllFromHttpPairs());
+
 $html = file_get_contents("apiTestHtmlTemplate.html");
 echo $html;
 
-echo MySqlDbCrudOperations::getResultSetAsHtmlString(MySqlDbCrudOperations::getResultSetForSelectAllFromHttpPairs());
+
 
 ?>
