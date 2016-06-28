a729d3ba72094b6338866b7b752384f7826cbbcb
Kiren Padayachee <Kiren.Padayachee@multichoice.co.za>
2016-06-28 13:10:41 +0200

    Modified API Test page to include parameters and reloading of the DB view

--- apiTest.php
+++ apiTest.php
@@ -3,8 +3,6 @@
 include("mysqldb/mySqlDbInit.php");
 include("mysqldb/mySqlDbCrudOperations.php");
 
-echo MySqlDbCrudOperations::getResultSetAsHtmlString(MySqlDbCrudOperations::getResultSetForSelectAllFromHttpPairs());
-
 $html = file_get_contents("apiTestHtmlTemplate.html");
 echo $html;
 
